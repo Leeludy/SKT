f3c770d665fc70e838eb647771de89463dd27e35
TF <tf@example.com>
2022-04-21 10:26:15 +0000

    SKT-97

--- src/components/Equipment/EquipmentList.js
+++ src/components/Equipment/EquipmentList.js
@@ -1,30 +1,45 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Function to load and render Equipment component
 function EquipmentList() {
 
-  // View Render - Table equipment
-  return (
-    <div className="container py-5">
-      <div className="row">
-        <div className="col-md-12">
-          <div className="card">
-            <div className="card-header">
-              <h3 className="card-title">List</h3>
-            </div>
-            <div className="card-body">
-              <div className="row">
+    const [equipmentList, setEquipmentList] = useState([]);
+
+    // Make API request to get equipment
+    useEffect(() => {
+        axios.get('http://localhost:3030/equipment')
+            .then((res) => {
+                setEquipmentList(res.data);
+            })
+            .catch((err) => {
+                throw err;
+            });
+    }, []);
+
+    // View Render - Table equipment
+    return (
+        <div className="container py-5">
+            <div className="row">
                 <div className="col-md-12">
-                  {/* Render list equipment */}
+                    <div className="card">
+                        <div className="card-header">
+                            <h3 className="card-title">List</h3>
+                        </div>
+                        <div className="card-body">
+                            <div className="row">
+                                <div className="col-md-12">
+                                    {/* Render list equipment */}{console.log(equipmentList)}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
-  
+    );
+
 };
 
 export default EquipmentList;
